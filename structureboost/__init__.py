0351876edad3ebda0ca78631c69fd39b4c22959e
blucena <brianlucena@gmail.com>
2020-06-01 15:22:33 -0700

    0.0.7

--- structureboost/__init__.py
+++ structureboost/__init__.py
@@ -2,8 +2,8 @@
 
 import graphs
 from structure_gb import StructureBoost
-from structure_gb_multiclass import StructureBoostMulticlass
-from structure_gb_mc import StructureBoostMC
+# from structure_gb_multiclass import StructureBoostMulticlass
+# from structure_gb_mc import StructureBoostMC
 from .utils import get_basic_config, apply_defaults, default_config_dict, ice_plot
 
 __version__ = '0.0.7'
